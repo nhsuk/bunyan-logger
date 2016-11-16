667584828d826ffadb807a2737497f571728b190
steven hunt <st3v3n.hunt@gmail.com>
2016-11-16 18:21:31 +0000

    :bell: Make sure name is supplied

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const loggerUtils = require('./lib/loggerUtils');
 const bunyan = require('bunyan');
+const assert = require('assert');
 
 const env = process.env.NODE_ENV || 'development';
 
 function log(name) {
+  assert(name, 'name must not be empty');
   return bunyan.createLogger({
     name,
     serializers: bunyan.stdSerializers,
--- test/unit/index.js
+++ test/unit/index.js
@@ -1,12 +1,26 @@
 const chai = require('chai');
-
-const loggerName = 'logger-name';
-const logger = require('../../index')(loggerName);
+const AssertionError = require('assert').AssertionError;
 
 const expect = chai.expect;
 
 describe('logger', () => {
   it('should be able to be named', () => {
+    const loggerName = 'logger-name';
+    // eslint-disable-next-line global-require
+    const logger = require('../../index')(loggerName);
+
     expect(logger.fields.name).to.equal(loggerName);
+  });
+
+  it('should throw an exception when no name is provided', () => {
+    // eslint-disable-next-line global-require
+    expect(() => { require('../../index')(); })
+    .to.throw(AssertionError, 'name must not be empty');
+  });
+
+  it('should throw an exception when an empty name is provided', () => {
+    // eslint-disable-next-line global-require
+    expect(() => { require('../../index')(''); })
+    .to.throw(AssertionError, 'name must not be empty');
   });
 });
