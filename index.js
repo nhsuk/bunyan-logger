3d67a8dfece00f7db29946bebb23be97e42ea975
Steven Hunt <st3v3n.hunt@gmail.com>
2017-03-15 11:18:34 +0000

    :put_litter_in_its_place: Default to stdout stream
    No longer use the `splunk-bunyan-logger` in production. Or anywhere,
    completely remove it due to different way of collecting logs.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function log(name) {
     name,
     serializers: bunyan.stdSerializers,
     level: loggerUtils.getLogLevel(env),
-    streams: loggerUtils.getStreams(env),
+    streams: [{ stream: process.stdout }],
   });
 }
 
